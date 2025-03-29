ee30193eb7227693436fb83943b005e82a6c8972
lavanyagupta7 <lovie2303@gmail.com>
2025-03-29 11:26:59 +0530

    Update 1ds22cd025.py
    check if reversible

--- utilities/1ds22cd025.py
+++ utilities/1ds22cd025.py
@@ -1,1 +1,14 @@
-hello abc
+#check if reversible
+
+a=int(input("enter a 3 digit number"))
+t=0
+while(a>0):
+    r= a%10
+    t=t+r
+    a=a/10
+
+if(t==r):
+    print("reversible")
+
+else:
+    print("irreversible")
