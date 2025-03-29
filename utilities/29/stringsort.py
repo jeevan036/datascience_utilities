0f3f47c71101bef8ae037ce3ebccbf0de693c3ac
rohini1504 <44237192+rohini1504@users.noreply.github.com>
2025-03-29 11:09:06 +0530

    Create stringsort.py
    code to sort a given input string

---
+++ utilities/29/stringsort.py
@@ -0,0 +1,7 @@
+def sort_string():
+    string = input("Enter a string: ")
+    sorted_string = ''.join(sorted(string))
+    print("Sorted string:", sorted_string)
+
+if __name__ == "__main__":
+    sort_string()
